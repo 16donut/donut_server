8cc5e617bac08d89ddc28f941b057672dbd2a3fb
holmir97 <holmir97@gmail.com>
2021-05-11 17:41:56 +0900

    내 정보 조회 5.11

--- src/controller/mypageController.js
+++ src/controller/mypageController.js
@@ -4,12 +4,56 @@ const { verify } = require('../library/jwt');
 
 const mypageService = require('../service/mypageService');
 
+/* 내 정보 조회
+    user = userIdx, user_id, user_name, user_access_dt
+    1. 아이디가 존재하지 않을 경우
+*/
 async function getMypageInfo(req, res) {  // 내정보 조회
+    try{
+        const token = req.headers.authorization;
+        console.log(token);
+
+        const decoded = verify(token);
+        console.log(decoded);
+
+        const userIdx = decoded.userIdx;
+        console.log(userIdx);
+
+        // req: mypageService 에서 받아온 유저정보
+        const userInfo = await mypageService.getMypageInfo(req.body);
+
+        if(token)
+        // 요청 바디가 없을 경우
+        if(userInfo == -1){
+            errResponse(res, returnCode.BAD_REQUEST, '요청된 데이터가 없습니다.');
+        }
+
+        else if(userInfo == -2){    //해당 유저 정보가 없을 경우
+            response(res, returnCode.BAD_REQUEST, "등록된 유저 정보가 없습니다");
+        }
+        else{
+            response(res,returnCode.OK, '유저정보 조회 성공', userInfo);
+        }
+    }catch(error){
+        console.log(error.message);
+        errResponse(res,returnCode.INTERNAL_SERVER_ERROR ,"서버 오류");
+    }
 }
+
+
 async function getMypagePrescription(req, res) {  // 처방전목록조회(최신순)
+    try{
+        // req는 해당 유저키를 이용하여 DAO에서 처리 후 객체 반환
+        const [prescription] = await getMypagePrescription.getMypagePrescription(req)
+
+    }catch(error){
+        console.log(error);
+        errResponse(res, returnCode.DB_ERROR, "처방전 목록이 없습니다")
+    }
 }
 async function postMypageMedicine(req, res) {  // 1개의처방전약품조회(가나다)
 }
+
 
 module.exports = { 
     getMypageInfo,
--- src/dao/mypageDao.js
+++ src/dao/mypageDao.js
@@ -1,9 +1,32 @@
 const mysql = require('../library/mysql');
 
-/*
-쿼리문 작성
-*/
+// user 데이터 조회
+async function selectUserInfo(user_id) {    // 쿼리에 데이터를 넣어야 하는 경우
+    const selectUserSql = `SELECT user_id, user_name FROM USER WHERE user_id = ?`;
+    return await mysql.query(selectUserSql, [user_id]);
+}
 
+// 처방전 리스트 조회
+async function selectPrescriptionList(user_id){
+    const [selectList] = `SELECT prescriptionIdx FROM PRESCRIPTION WHERE user_id = ? ORDER BY prescription_dt DESC`;
+    return await mysql.query(selectList, [user_id]);
+}
+
+
+// 1개의 처방전 약품조회
+// async function selectMyPagePrescription(user_id){
+//     const [selectUserId] = `SELECT prescriptionIdx FROM PRESCRIPTION WHERE = ?`;
+//     const user_prescription = await mysql.query(selectUserId, [user_id]);
+
+
+//     const selectMovieSql = `SELECT allMedicineIdx, total_dose_dt, my_dose_dt FROM PRESCRIPTION WHERE = ?`;
+//     return await mysql.query(selectMovieSql, [user_prescription]);
+    
+//     const selectMovieSql = `SELECT prescriptionIdx ,allMedicineIdx, total_dose_dt, my_dose_dt FROM PRESCRIPTION, USER WHERE USER.user_id = PRESCRIPTION.user_id and PRESCRIPTION.user_id = ?`;
+//     return await mysql.query(selectMovieSql, [user_prescription]);
+// }
 module.exports = {
-    selectTestList
+    selectUserInfo,
+    selectPrescriptionList
+    // selectMyPagePrescription
 }
